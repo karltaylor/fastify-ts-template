5cab1c92086a3970265e104ee076ad5db473a5bd
Karl <karltaylor@users.noreply.github.com>
2023-09-19 10:40:24 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import fastify from "fastify";
 
 const server = fastify();
 
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
-});
+server.get("/", (request, reply) => reply.send({ ok: true }));
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
