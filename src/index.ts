bf909b7ef826f3902662a65e29bc704bd2984105
karl <karl@studio206.dev>
2024-04-18 11:27:32 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { env } from "./env";
 const server = fastify();
 
 server.get("/", (request, reply) => 
-  reply.send({ service: "fast-fy-ts-template" })
+  reply.send({ service: "fastify-ts-template" })
 );
 
 server.listen({ port: env.PORT, host: '127.0.0.1' }, (err, address) => {
